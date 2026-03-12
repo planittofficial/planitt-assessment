e161f7d8db256a95d94108480b61241ebc986402
Ansh Mishra <anshm.cse22@sbjit.edu.in>
2026-03-12 19:31:38 +0530

    fuck you backend

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <html>
+      <body>
+        <h1>404 - Page Not Found</h1>
+        <a href="/login">Return Login</a>
+      </body>
+    </html>
+  );
+}
